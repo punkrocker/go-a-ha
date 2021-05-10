c774496cd734ed2598c12072382eb49c29ac2aa8
yuhaokui <yuhaokui@healai.com>
2021-05-10 14:18:03 +0800

    2.2 解密回文——栈

--- stack/checkPalindrome.go
+++ stack/checkPalindrome.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import "fmt"
 
+const checkMark = "\u2713"
+const errorMark = "\u2717"
+
+func main() {
+	check(false, isPalindrome("test"), "test should be false")
+	check(true, isPalindrome("aba"), "aba should be true")
+	check(true, isPalindrome("aa"), "aa should be true")
+}
+
+func check(want bool, real bool, message string) {
+	if want == real {
+		fmt.Println(message, checkMark)
+	} else {
+		fmt.Println(message, errorMark)
+	}
+}
+
+func isPalindrome(s string) bool {
+	for i := 0; i < len(s)/2; i++ {
+		if s[i] != s[len(s)-1-i] {
+			return false
+		}
+
+	}
+	return true
 }
